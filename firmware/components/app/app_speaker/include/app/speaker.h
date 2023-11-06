5ba926eb0427399986183180425a95f0f29da2a7
Definev <daiduong.workmail@gmail.com>
2023-11-06 18:52:17 +0700

    refactor: Update speaker setup and write to i2s legacy

--- firmware/components/app/app_speaker/include/app/speaker.h
+++ firmware/components/app/app_speaker/include/app/speaker.h
@@ -4,10 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/i2s.h"
 
-#include "sdkconfig.h"
+void speaker_setup(void);
 
-void speaker_setup(i2s_chan_handle_t *speaker_handle, int32_t *wav_data);
+void speaker_write(char *data, size_t *bytes_written);
 
-void speaker_write(i2s_chan_handle_t speaker_handle, char *data, size_t *bytes_written);
-
-    #endif // SPEAKER_H
+#endif // SPEAKER_H
--- firmware/components/app/app_speaker/speaker.c
+++ firmware/components/app/app_speaker/speaker.c
@@ -1,8 +1,10 @@
-// #include <stdlib.h>
+#include <stdlib.h>
 
-// #include "sdkconfig.h"
-// #include "app/i2s.h"
-// #include "app/speaker.h"
+#include "sdkconfig.h"
+#include "app/i2s.h"
+#include "app/speaker.h"
+
+#include "esp_log.h"
 
 // #include "freertos/FreeRTOS.h"
 // #include "driver/i2s.h"
@@ -10,55 +12,76 @@
 // #include "esp_log.h"
 // #include "esp_heap_caps.h"
 
-// // how many samples to load from TCP before starting playing (avoid jitter due to running out of data w/ bad wifi)
-// #ifdef CONFIG_SOC_GDMA_SUPPORT_PSRAM
-// int bufferThreshold = 8192;
-// #else
-// int bufferThreshold = 512;
-// #endif
+// how many samples to load from TCP before starting playing (avoid jitter due to running out of data w/ bad wifi)
+#ifdef CONFIG_SPIRAM_SIZE
+int bufferThreshold = 8192;
+#else
+int bufferThreshold = 512;
+#endif
 
-// static const char *TAG = "speaker";
+static const char *TAG = "speaker";
 
-// void speaker_setup(int32_t *wav_data)
-// {
-//     /* Initialize WAV form */
-//     size_t free_psram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
-//     size_t total_psram = heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
+void speaker_setup(void)
+{
+    /* Initialize WAV form */
+    // size_t free_psram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+    // size_t total_psram = heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
 
-//     ESP_LOGI(TAG, "PSRAM free: %d", free_psram);
-//     ESP_LOGI(TAG, "PSRAM total: %d", total_psram);
-//     ESP_LOGI(TAG, "PSRAM used: %d", total_psram - free_psram);
+    // ESP_LOGI(TAG, "PSRAM free: %d", free_psram);
+    // ESP_LOGI(TAG, "PSRAM total: %d", total_psram);
+    // ESP_LOGI(TAG, "PSRAM used: %d", total_psram - free_psram);
 
-//     wav_data = (int32_t *)malloc((2 * 1024 * 1024) / sizeof(int32_t));
-//     if (wav_data == NULL)
-//     {
-//         ESP_LOGE(TAG, "Memory allocation failed!");
-//         while (1)
-//             ;
-//     }
-//     else
-//     {
-//         ESP_LOGI(TAG, "Memory allocation successful!");
-//     }
-//     free_psram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
-//     ESP_LOGI(TAG, "PSRAM used: %d", total_psram - free_psram);
+    // wav_data = (int32_t *)malloc((2 * 1024 * 1024) / sizeof(int32_t));
+    // if (wav_data == NULL)
+    // {
+    //     ESP_LOGE(TAG, "Memory allocation failed!");
+    //     while (1)
+    //         ;
+    // }
+    // else
+    // {
+    //     ESP_LOGI(TAG, "Memory allocation successful!");
+    // }
+    // free_psram = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
+    // ESP_LOGI(TAG, "PSRAM used: %d", total_psram - free_psram);
 
-//     /* Initialize speaker */
-//     i2s_chan_config_t speaker_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
-//     ESP_ERROR_CHECK(i2s_new_channel(&speaker_chan_cfg, speaker_handle, NULL));
+    /* Initialize speaker */
 
-//     i2s_std_config_t speaker_std_cfg = {
-//         .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(I2S_SAMPLE_RATE),
-//         .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_BIT_PER_SAMPLE, I2S_MONO_OR_STEREO),
-//         .gpio_cfg = {
-//             .bclk = I2S_SERIAL_CLOCK,
-//             .ws = I2S_WORD_SELECT,
-//             .dout = I2S_SPEAKER_SERIAL_DATA,
-//         },
-//     };
-//     ESP_ERROR_CHECK(i2s_channel_init_std_mode(*speaker_handle, &speaker_std_cfg));
-//     ESP_ERROR_CHECK(i2s_channel_enable(*speaker_handle));
-// }
+    const i2s_config_t i2s_config = {
+        .mode = (I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = I2S_SAMPLE_RATE,
+        .bits_per_sample = I2S_SPEAKER_BIT_PER_SAMPLE,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags = 0,
+        .dma_buf_count = I2S_DMA_DESC_NUM,
+        .dma_buf_len = I2S_DMA_FRAME_NUM,
+        .use_apll = false,
+    };
+
+    const i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_SERIAL_CLOCK,
+        .ws_io_num = I2S_WORD_SELECT,
+        .data_out_num = I2S_SPEAKER_SERIAL_DATA,
+        .data_in_num = -1,
+    };
+
+    i2s_driver_install(I2S_SPEAKER_PORT, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_SPEAKER_PORT, &pin_config);
+    i2s_start(I2S_SPEAKER_PORT);
+
+    ESP_LOGI(TAG, "Start new speaker:\n");
+    ESP_LOGI(TAG, "- Sample rate: %d", I2S_SAMPLE_RATE);
+    ESP_LOGI(TAG, "- Serial clock: %d", I2S_SERIAL_CLOCK);
+    ESP_LOGI(TAG, "- Word select: %d", I2S_WORD_SELECT);
+    ESP_LOGI(TAG, "- Data line: %d", I2S_SPEAKER_SERIAL_DATA);
+    ESP_LOGI(TAG, "speaker initialized");
+}
+
+void speaker_write(char *data, size_t *bytes_written)
+{
+    ESP_ERROR_CHECK(i2s_write(I2S_SPEAKER_PORT, data, I2S_BUFFER_SIZE, bytes_written, portMAX_DELAY));
+}
 
 // void speaker_write(char *data, size_t *bytes_written)
 // {
