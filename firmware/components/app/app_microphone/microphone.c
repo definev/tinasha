2ea2556749bf5f49756b415501ea2c08bd00be8c
Definev <daiduong.workmail@gmail.com>
2023-11-06 18:52:54 +0700

    chore: Update I2S configuration for microphone and speaker

--- firmware/components/app/app_microphone/microphone.c
+++ firmware/components/app/app_microphone/microphone.c
@@ -11,7 +11,7 @@ static const char *TAG = "microphone";
 
 void transform_mic_data(int16_t *data, size_t size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size / sizeof(int16_t); i++)
     {
         data[i] = (int16_t)data[i] << 3;
     }
@@ -20,7 +20,6 @@ void microphone_setup(void)
 {
     const i2s_config_t i2s_config = {
         .mode = (I2S_MODE_MASTER | I2S_MODE_RX),
-        //.sample_rate = 44100,
         .sample_rate = I2S_SAMPLE_RATE,
         .bits_per_sample = I2S_BIT_PER_SAMPLE,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
@@ -31,29 +30,28 @@ void microphone_setup(void)
         .use_apll = false,
     };
 
-    i2s_driver_install(I2S_MIC_PORT, &i2s_config, 0, NULL);
-
     const i2s_pin_config_t pin_config = {
         .bck_io_num = I2S_SERIAL_CLOCK,
         .ws_io_num = I2S_WORD_SELECT,
         .data_out_num = -1,
-        .data_in_num = I2S_MIC_SERIAL_DATA,
+        .data_in_num = I2S_MICROPHONE_SERIAL_DATA,
     };
 
-    i2s_set_pin(I2S_MIC_PORT, &pin_config);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    i2s_start(I2S_MIC_PORT);
+    i2s_driver_install(I2S_MICROPHONE_PORT, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_MICROPHONE_PORT, &pin_config);
+    i2s_start(I2S_MICROPHONE_PORT);
 
     ESP_LOGI(TAG, "Start new microphone:\n");
     ESP_LOGI(TAG, "- Sample rate: %d", I2S_SAMPLE_RATE);
     ESP_LOGI(TAG, "- Serial clock: %d", I2S_SERIAL_CLOCK);
     ESP_LOGI(TAG, "- Word select: %d", I2S_WORD_SELECT);
-    ESP_LOGI(TAG, "- Data line: %d", I2S_MIC_SERIAL_DATA);
+    ESP_LOGI(TAG, "- Data line: %d", I2S_MICROPHONE_SERIAL_DATA);
     ESP_LOGI(TAG, "microphone initialized");
 }
 
 void microphone_read(void *mic_buffer, size_t buffer_size, size_t *bytes_read)
 {
+    transform_mic_data(mic_buffer, buffer_size);
     // ESP_ERROR_CHECK(i2s_channel_read(handler, mic_buffer, buffer_size, bytes_read, portMAX_DELAY));
-    i2s_read(I2S_MIC_PORT, mic_buffer, buffer_size, bytes_read, portMAX_DELAY);
+    i2s_read(I2S_MICROPHONE_PORT, mic_buffer, buffer_size, bytes_read, portMAX_DELAY);
 }
