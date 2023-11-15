fc1fa8a341e385defcb53b90d455a67d65958e0f
tripercy <percytri37@gmail.com>
2023-11-16 00:54:08 +0700

    Added load context and answer in server

--- pdf_server/server.py
+++ pdf_server/server.py
@@ -1,53 +1,130 @@
 #!/usr/bin/env python
 import json
-from typing import List
 
 from fastapi import FastAPI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.prompts.base import StringPromptValue
 
-from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import GooglePalmEmbeddings
-from langchain.chat_models import ChatGooglePalm
-import google.generativeai as palm
+from langchain.llms import GooglePalm
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
 
-from langchain.schema import BaseOutputParser
+from langchain.chains.question_answering import load_qa_chain
 from langserve import add_routes
 
+from langchain.schema.runnable import RunnableLambda
+
+from PyPDF2 import PdfReader
+from operator import itemgetter
+
+# MODEL SETUP
 with open("credentials.json", "r") as f:
     cred = json.load(f)
     api_key = cred.get("google_api_key")
 
-class CommaSeparatedListOutputParser(BaseOutputParser[List[str]]):
-    """Parse the output of an LLM call to a comma-separated list."""
+defaults = {
+    "model": "models/text-bison-001",
+    "temperature": 1,
+    "candidate_count": 1,
+    "top_k": 40,
+    "top_p": 0.95,
+    "max_output_tokens": 1024,
+    "stop_sequences": [],
+    "safety_settings": [
+        {"category": "HARM_CATEGORY_DEROGATORY", "threshold": 1},
+        {"category": "HARM_CATEGORY_TOXICITY", "threshold": 1},
+        {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 2},
+        {"category": "HARM_CATEGORY_SEXUAL", "threshold": 2},
+        {"category": "HARM_CATEGORY_MEDICAL", "threshold": 2},
+        {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 2},
+    ],
+}
 
+llm = GooglePalm(google_api_key=api_key, **defaults)
+# ====================
 
-    def parse(self, text: str) -> List[str]:
-        """Parse the output of an LLM call."""
-        return text.strip().split(", ")
+# LOAD PDF
 
-template = """You are a helpful assistant who generates comma separated lists.
-A user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
-ONLY return a comma separated list, and nothing more."""
-human_template = "{text}"
+def process_text(text):
+    # Split the text into chunks using Langchain's CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
 
-chat_prompt = ChatPromptTemplate.from_messages([
-    ("system", template),
-    ("human", human_template),
-])
-category_chain = chat_prompt | ChatGooglePalm(google_api_key=api_key) | CommaSeparatedListOutputParser()
+    embeddings = GooglePalmEmbeddings(google_api_key=api_key)
+    knowledgeBase = FAISS.from_texts(chunks, embeddings)
 
-# 2. App definition
-app = FastAPI(
-  title="LangChain Server",
-  version="1.0",
-  description="A simple api server using Langchain's Runnable interfaces",
+    return knowledgeBase
+
+text = ""
+
+knowledgeBase = None
+
+chain = load_qa_chain(llm, chain_type="stuff")
+
+def handle_question(question: StringPromptValue):
+    question = question.to_string()
+    if len(text) == 0:
+        return "No document loaded yet. Please load a document first."
+    
+    docs = knowledgeBase.similarity_search(question)
+    response = chain.run(input_documents=docs, question=question)
+
+    return response
+
+prompt_format = "Using the given document, answer the following question: {question}"
+
+prompt = PromptTemplate(
+    input_variables=["question"],
+    template=prompt_format,
 )
 
-# 3. Adding chain route
+pdf_ask_chain = (
+    prompt | RunnableLambda(handle_question)
+)
+
+
+app = FastAPI(
+    title="Test PDF Server",
+    version="1.0",
+    description="A simple api server to chat with PDF",
+)
+
+# Ask PDF route
 add_routes(
     app,
-    category_chain,
-    path="/category_chain",
+    pdf_ask_chain,
+    path="/ask_pdf",
+)
+
+# Add PDF route
+add_pdf_format = "{content}"
+
+add_prompt = PromptTemplate(
+    input_variables=["content"],
+    template=add_pdf_format,
+)
+
+def add_pdf(content: StringPromptValue):
+    global text
+    text = content.to_string()
+    global knowledgeBase
+    knowledgeBase = process_text(text)
+    return "PDF added"
+
+pdf_add_chain = (
+    add_prompt | RunnableLambda(add_pdf)
+)
+
+add_routes(
+    app,
+    pdf_add_chain,
+    path="/add_pdf",
 )
 
 if __name__ == "__main__":
